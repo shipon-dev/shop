c05a67f2eee90a9eea82601918cbe33d0774482f
Shipon <mdshiponmolla3@gmail.com>
2024-03-27 22:09:18 +0600

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,16 +4,10 @@ import EditScreenInfo from '../../components/edit-screen-info';
 
 export default function TabOneScreen() {
   return (
-    <View className={styles.container}>
-      <Text className={styles.title}>Tab One</Text>
-      <View className={styles.separator} />
+    <View className="items-center flex-1 justify-center">
+      <Text className="text-xl font-bold">Tab One</Text>
+      <View className="h-[1px] my-7 w-4/5 bg-gray-200" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
-
-const styles = {
-  container: `items-center flex-1 justify-center`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
-};
